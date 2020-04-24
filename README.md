d44ec79b6c87006230be84dccf1e58a1c8ae870a
P.J. Finlay <admin@argosopentech.com>
2020-04-24 19:20:00 -0400

    Wrote script to do some basic cleaning up

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Vocabulary for learners of a new language, scraped from the public domain book [Esperanto Self-Taught with Phonetic Pronunciation by William W. Mann](https://www.gutenberg.org/ebooks/23984).
 
+words.csv contains 3500+ words.
+
---
+++ src/script.py
@@ -0,0 +1,36 @@
+import csv
+import re
+
+# Read in csv file into a list of words with titles intermixed
+input_file = open('Esperanto Self-Taught Vocab.csv', 'r')
+csv_reader = csv.reader(input_file)
+rows = list(csv_reader)
+rows = [row[0] for row in rows]
+
+# Cleanup junk that got copied and pasted in
+rows = [re.sub(r'\(.*\)', '', row) for row in rows]
+rows = [re.sub(r'\[.*\]', '', row) for row in rows]
+rows = [re.sub(r'—', '', row) for row in rows]
+rows = [re.sub(r',', '', row) for row in rows]
+rows = [re.sub(r';', '', row) for row in rows]
+rows = [re.sub(r'"', '', row) for row in rows]
+rows = [re.sub(r'\'', '', row) for row in rows]
+
+# Remove rows without any words
+def more_than_one_alnum_char(x):
+    return len(''.join(c for c in x if c.isalnum())) > 0
+cleaned_words = filter(more_than_one_alnum_char, rows)
+cleaned_words = [word.strip() for word in cleaned_words]
+cleaned_words = filter(lambda x: len(x) > 0, cleaned_words)
+cleaned_words = list(cleaned_words)
+
+# Split into vocab and phrases sections
+phrases_index = cleaned_words.index('Useful and Necessary Expressions.')
+vocab_words = cleaned_words[1:phrases_index]
+phrases = cleaned_words[phrases_index + 1:]
+
+# Write into csv file
+with open('words.csv', 'w') as output_file:
+    csv_writer = csv.writer(output_file)
+    for row in cleaned_words:
+        csv_writer.writerow([row])
