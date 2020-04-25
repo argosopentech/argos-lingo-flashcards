3208d13ad749910c2b3f47bcfbe921b846711072
P.J. Finlay <admin@argosopentech.com>
2020-04-25 11:32:45 -0400

    Improved the cleaning script

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 Vocabulary for learners of a new language, scraped from the public domain book [Esperanto Self-Taught with Phonetic Pronunciation by William W. Mann](https://www.gutenberg.org/ebooks/23984).
 
-words.csv contains 3500+ words and phrases.
+all_words.csv contains 3500+ words and phrases and the csv directory contains all of the vocab words broken up into named lessons.
 
--- src/clean.py
+++ src/clean.py
@@ -5,6 +5,7 @@
 
 import csv
 import re
+import os
 
 # Read in csv file into a list of words with titles intermixed
 input_file = open('Esperanto Self-Taught Vocab.csv', 'r')
@@ -21,6 +22,7 @@ rows = [re.sub(r';', '', row) for row in rows]
 rows = [re.sub(r'"', '', row) for row in rows]
 rows = [re.sub(r'\'', '', row) for row in rows]
 rows = [re.sub(r'English\.', '', row) for row in rows]
+rows = [re.sub(r'&', 'and', row) for row in rows]
 
 # Remove rows without any words
 def more_than_one_alnum_char(x):
@@ -35,8 +37,42 @@ phrases_index = cleaned_words.index('Useful and Necessary Expressions.')
 vocab_words = cleaned_words[1:phrases_index]
 phrases = cleaned_words[phrases_index + 1:]
 
-# Write into csv file
-with open('words.csv', 'w') as output_file:
+# Create lessons out of word list
+lessons = []
+class Lesson:
+    title = ''
+    words = []
+    def __str__(self):
+        return self.title + '\n' + '-' * 10 + '\n' + '\n'.join(self.words) 
+current_lesson = None
+for regex, words in [(r'^[0123456789]*\. ', vocab_words)]:
+    for word in words:
+        if re.search(regex, word) != None:
+            # This row is a title
+            word = re.sub(regex, '', word)
+            if current_lesson != None:
+                lessons.append(current_lesson)
+            current_lesson = Lesson()
+            current_lesson.title = word
+            current_lesson.words = []
+        else:
+            current_lesson.words.append(word)
+
+# Write to files
+output_dir = 'output'
+def write_list_to_csv(filename, rows):
+    with open(filename, 'w') as output_file:
+        csv_writer = csv.writer(output_file)
+        for row in rows:
+            csv_writer.writerow([row])
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+if not os.path.exists(output_dir + '/csv'):
+    os.makedirs(output_dir + '/csv')
+for lesson in lessons:
+    write_list_to_csv(output_dir + '/csv/' + lesson.title + 'csv', lesson.words)
+
+with open(output_dir + '/all_words.csv', 'w') as output_file:
     csv_writer = csv.writer(output_file)
     for row in cleaned_words:
         csv_writer.writerow([row])
